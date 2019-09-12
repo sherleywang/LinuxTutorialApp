dfa0e0d13fa3c6546f6b36f55b172119c5bb09f8
sherleywang <sherleywang@ymail.com>
2019-09-12 00:47:26 -0500

    Added basic functions and structure to the Terminal class

--- app/src/main/java/androidapps/linuxtutorialapp/Terminal.java
+++ app/src/main/java/androidapps/linuxtutorialapp/Terminal.java
@@ -1,6 +1,48 @@
 package androidapps.linuxtutorialapp;
 
+import android.widget.EditText;
+import android.widget.TextView;
+
 // a class for emulating the terminal
 public class Terminal {
 
+    private String content;
+    private String username;
+
+    private EditText input;
+
+    private TextView display;
+
+    public Terminal(String username, TextView display, EditText input) {
+        content = "";
+        this.username = username;
+        this.display = display;
+        this.input = input;
+    }
+
+    // call this method upon pressing enter/submit
+    /* Note to Ace: used a parameter for the String rather than an instance variable so I could
+    prevent bugs/errors from editing the String and updating the terminal when it's not necessary */
+    public void submit() {
+        String text = input.getText().toString();
+        updateTerminal(text);
+    }
+
+    // update the display of the terminal
+    private void updateTerminal(String text) {
+        content += "\n";
+        addUserResponse(text);
+        content += "\n";
+        addTerminalResponse(text);
+    }
+
+    private void addUserResponse(String text) {
+        // change header as necessary
+        String header = username + "$ ";
+        content += header + text;
+    }
+
+    private void addTerminalResponse(String text) {
+        // might require another class?
+    }
 }
