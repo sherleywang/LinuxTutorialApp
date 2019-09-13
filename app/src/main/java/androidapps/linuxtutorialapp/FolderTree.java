0c94cb6227be3fb2472d7e5eb8e520cbf81fd0ea
Lmfaoown <AceJamshidi4th@gmail.com>
2019-09-12 20:48:37 -0400

    Finished Node class, started Tree class

--- app/src/main/java/androidapps/linuxtutorialapp/FolderTree.java
+++ app/src/main/java/androidapps/linuxtutorialapp/FolderTree.java
@@ -2,14 +2,22 @@ package androidapps.linuxtutorialapp;
 
 import java.util.ArrayList;
 
+@SuppressWarnings("all")
 public class FolderTree {
+
+    private Node<String> root;
+
+    public boolean add(Node<String> value) {
+
+        return false;
+    }
 
     // class that implements the nodes for the tree
     private static class Node<E> {
 
         private E info;
-        private Node<E> parent = null;
-        private ArrayList<Node<E>> children = null;
+        private Node<E> parent;
+        private ArrayList<Node<E>> children;
 
         public Node(E value) {
             info = value;
@@ -37,6 +45,10 @@ public class FolderTree {
 
         public void addChild(Node<E> child) {
             children.add(child);
+        }
+
+        public void deleteChild(int child) {
+            children.remove(child);
         }
     }
 }
