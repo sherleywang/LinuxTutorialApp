eaa24432193c320b383741fb7d3ad17a76c9122c
sherleywang <sherleywang@ymail.com>
2019-09-17 22:49:55 -0500

    -Deleted Node class -Moved Node as a nested class in FolderTree

--- app/src/main/java/androidapps/linuxtutorialapp/FolderTree.java
+++ app/src/main/java/androidapps/linuxtutorialapp/FolderTree.java
@@ -67,4 +67,47 @@ public class FolderTree {
         }
     }
 
+    // tree node class to support the folder directory implementation
+    private static class Node<E> {
+
+        private E info;
+        private Node<E> parent;
+        private HashMap<E, Node<E>> children;
+
+        public Node(E value) {
+            info = value;
+            children = new HashMap<>();
+        }
+
+        public E getInfo() {
+            return info;
+        }
+
+        public void setInfo(E value) {
+            info = value;
+        }
+
+        public Node<E> getParent() {
+            return parent;
+        }
+
+        public void setParent(Node<E> parent) {
+            this.parent = parent;
+        }
+
+        public HashMap<E, Node<E>> getChildren() {
+            return children;
+        }
+
+        public void addChild(Node<E> child) {
+            children.put(child.info, child);
+        }
+
+        public void deleteChild(int child) {
+            if (child >= children.size() || child < 0)
+                throw new IllegalArgumentException("Index is out of bounds.");
+            children.remove(child);
+        }
+    }
+
 }
